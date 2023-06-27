e200e5b4bea639216e18f987dc4c2ebb583930f5
Justin Liew <jliew@fastly.com>
2023-06-27 13:50:08 -0700

    bind to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -177,7 +177,7 @@ async fn hello(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0,0,0,0], 3000));
 
     let listener = TcpListener::bind(addr).await?;
 
